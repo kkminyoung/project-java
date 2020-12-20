04dba382f237af9d9f38ac8511f047c93251cf83
Kim Minyoung <61506233+kkminyoung@users.noreply.github.com>
2020-12-20 17:20:45 +0900

    Update ServerIP.java

--- src/server/ServerIP.java
+++ src/server/ServerIP.java
@@ -1,6 +1,6 @@
 package server;
 
-// ServerIP ¼³Á¤ 
+// ServerIP ì„¤ì •
 public interface ServerIP {
-	public static final String IP = "localhost"; //localhost·Î
+	public static final String IP = "localhost"; //localhostë¡œ
 }
